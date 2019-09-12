68bd252ad9d18feb18596368c1fec9aaa1a56461
Olga Radan <olga@dotdesignstudio.ro>
2019-09-12 11:52:57 +0300

    java script refactoring

--- assets/js/uStates.js
+++ assets/js/uStates.js
@@ -15,13 +15,14 @@
 	initMap()
 
 
-	uStates.draw = function (id, data, toolTip) {
+	uStates.draw = function (id,mapdata, data, toolTip) {
+		console.log(arguments)
 		width = document.documentElement.clientWidth;
 		if (width >= 1024) {
-			createSvgGroup(id, uStatePaths, sampleData, mouseOver, mouseOut)
+			createSvgGroup(id, mapdata, data, mouseOver, mouseOut)
 			addStatelabels(id)
 		} else if (width < 1024) {
-			createSvgGroup(id, uStatePaths, sampleData, open, close)
+			createSvgGroup(id, mapdata, data, open, close)
 			addStatelabels(id)
 		}
 
@@ -40,7 +41,7 @@
 		function open(d) {
 			var position
 			if (width - d3.event.pageX <= 150) {
-				position = 145 + 15
+				position = 135 + 15
 			} else {
 				position = 0
 			}
@@ -85,11 +86,7 @@
 	}
 
 	function tooltipHtml(n, d) {	/* function to create html content string in tooltip div. */
-		return `
-		<div class="d-flex align-items-center justify-content-between "><h4 class="mr-1">${n}</h4><svg class="fa-svg d-lg-none" id="close-btn" viewBox="0 0 259.5 260.1"><path d="M117.3,130.1L2.5,246c-3.2,3.2-3.2,8.4,0,11.7l0,0c3.1,3.2,8.3,3.2,11.5,0.1 c0,0,0.1-0.1,0.1-0.1l0,0l115.7-116.8l115.7,116.8c3.1,3.2,8.3,3.2,11.5,0.1c0,0,0.1-0.1,0.1-0.1l0,0c3.2-3.2,3.2-8.4,0-11.7l0,0 L142.1,130.1l114.9-116c3.2-3.2,3.2-8.4,0-11.7l0,0c-3.1-3.2-8.3-3.2-11.5-0.1c0,0,0,0-0.1,0.1l0,0L129.7,119.3L13.9,2.4 C10.8-0.8,5.7-0.8,2.5,2.3c0,0,0,0-0.1,0.1l0,0c-3.2,3.2-3.2,8.4,0,11.7l0,0L117.3,130.1z"></path></svg></div>
-		<table><tr><td>${d.providers.join("<br>")}</td></tr></table>
-		
-		`
+		return '<div class="d-flex align-items-center justify-content-between "><h6 class="mr-1">' + n + '</h6><svg class="fa-svg d-lg-none" id="close-btn" viewBox="0 0 259.5 260.1"><path d="M117.3,130.1L2.5,246c-3.2,3.2-3.2,8.4,0,11.7l0,0c3.1,3.2,8.3,3.2,11.5,0.1 c0,0,0.1-0.1,0.1-0.1l0,0l115.7-116.8l115.7,116.8c3.1,3.2,8.3,3.2,11.5,0.1c0,0,0.1-0.1,0.1-0.1l0,0c3.2-3.2,3.2-8.4,0-11.7l0,0 L142.1,130.1l114.9-116c3.2-3.2,3.2-8.4,0-11.7l0,0c-3.1-3.2-8.3-3.2-11.5-0.1c0,0,0,0-0.1,0.1l0,0L129.7,119.3L13.9,2.4 C10.8-0.8,5.7-0.8,2.5,2.3c0,0,0,0-0.1,0.1l0,0c-3.2,3.2-3.2,8.4,0,11.7l0,0L117.3,130.1z"></path></svg></div><table><tr><td>' + d.providers.join("<br>") + '</td></tr></table>'
 	}
 
 	function getMapData(urlObj) {
@@ -114,21 +111,12 @@
 	}
 
 
-	function initMap() {
-		getMapData(urlObj)
-			.then(function () {
-				createSampleData()
-			})
-			.then(function () {
-				uStates.draw("#statesvg", sampleData, tooltipHtml);
-			})
-	}
 
 	function createSampleData() {
 		uStatePaths.map(function (item) {
 			return item.id
 		}).forEach(function (d) {
-			var stateProviders = allProviders.find(e => {
+			var stateProviders = allProviders.find(function (e) {
 				return e.state === d
 			})
 			var low = Math.round(100 * Math.random())
@@ -136,4 +124,16 @@
 
 		})
 	}
+
+	function initMap() {
+		getMapData(urlObj)
+			.then(function () {
+				createSampleData()
+			})
+			.then(function () {
+				uStates.draw("#statesvg", uStatePaths, sampleData, tooltipHtml);
+			})
+	}
+
+
 })();
